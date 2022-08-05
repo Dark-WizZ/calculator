6879ffb0defd074d27b00e6ae9a5ebe4e2d3e9d1
Dark-WizZ <mailwithakram@gmail.com>
2022-08-06 01:24:52 +0530

    Add functions to the ui

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function add(a, b){return a+b}
 function sub(a, b){return a-b}
 function multiply(a, b){return a*b}
 function divide(a, b){return a/b}
-
 function operate(op, a, b){
   switch(op){
     case 'add' : return add(a, b);
@@ -11,16 +10,42 @@ function operate(op, a, b){
     case 'divide' : return divide(a, b);
   }
 }
-
 function numClick(){
+  if (afterOP) display.value=''
+  afterOP=false;
   display.focus();
   display.value += this.classList[0];
 }
+function opClick(){
+  afterOP=true;
+  if(operator==''){
+    display.value = display.value;
+  }else{
+    display.value = operate(operator, +input, +display.value); 
+  }
+  operator = (this.classList[0]=='equal')?'':this.classList[0];
+  input = display.value;
+}
+function clearClick(){
+  display.value='';
+  afterOP = false;
+  input = 0;
+  operator = '';
+}
 
-let input = '';
+let afterOP = false;
+let input = 0;
+let operator = '';
+
 const numBtns = document.querySelectorAll('.num');
 const display = document.querySelector('input');
+const opBtns = document.querySelectorAll('.op');
+const clear = document.querySelector('.clear');
 
 numBtns.forEach(btn =>{
   btn.addEventListener('click', numClick)
 })
+opBtns.forEach(btn=>{
+  btn.addEventListener('click', opClick)
+})
+clear.addEventListener('click', clearClick);
